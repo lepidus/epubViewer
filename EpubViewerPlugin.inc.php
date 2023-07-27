68fb53abb7d2404dbfaa08c4e9d0a408f3888c33
yves <yves@lepidus.com.br>
2023-07-27 15:57:53 -0400

    Add PKP_STRICT_MODE conditional and "/file.epub" to epubUrl.
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#599
    
    Signed-off-by: yves <yves@lepidus.com.br>

--- EpubViewerPlugin.inc.php
+++ EpubViewerPlugin.inc.php
@@ -10,6 +10,7 @@
  *
  * @brief This plugin enables embedding of ePUB viewer for epub display
  */
+
 namespace APP\plugins\generic\epubViewer;
 
 use PKP\plugins\Hook;
@@ -17,170 +18,182 @@ use PKP\plugins\GenericPlugin;
 use APP\core\Application;
 use APP\template\TemplateManager;
 
-class EpubViewerPlugin extends GenericPlugin {
-	/**
-	 * @copydoc Plugin::register()
-	 */
-	function register($category, $path, $mainContextId = null) {
-		if (parent::register($category, $path, $mainContextId)) {
-			if ($this->getEnabled($mainContextId)) {
-				Hook::add('ArticleHandler::view::galley', array($this, 'submissionCallback'), HOOK_SEQUENCE_LAST);
-				Hook::add('IssueHandler::view::galley', array($this, 'issueCallback'), HOOK_SEQUENCE_LAST);
-				Hook::add('CatalogBookHandler::view', [$this, 'viewCallback'], Hook::SEQUENCE_LATE);
+class EpubViewerPlugin extends GenericPlugin
+{
+    /**
+     * @copydoc Plugin::register()
+     */
+    public function register($category, $path, $mainContextId = null)
+    {
+        if (parent::register($category, $path, $mainContextId)) {
+            if ($this->getEnabled($mainContextId)) {
+                Hook::add('ArticleHandler::view::galley', array($this, 'submissionCallback'), HOOK_SEQUENCE_LAST);
+                Hook::add('IssueHandler::view::galley', array($this, 'issueCallback'), HOOK_SEQUENCE_LAST);
+                Hook::add('CatalogBookHandler::view', [$this, 'viewCallback'], Hook::SEQUENCE_LATE);
                 Hook::add('CatalogBookHandler::download', [$this, 'downloadCallback'], Hook::SEQUENCE_LATE);
-			}
-			return true;
-		}
-		return false;
-	}
+            }
+            return true;
+        }
+        return false;
+    }
 
-	/**
-	 * Install default settings on context creation.
-	 * @return string
-	 */
-	function getContextSpecificPluginSettingsFile() {
-		return $this->getPluginPath() . '/settings.xml';
-	}
+    /**
+     * Install default settings on context creation.
+     * @return string
+     */
+    public function getContextSpecificPluginSettingsFile()
+    {
+        return $this->getPluginPath() . '/settings.xml';
+    }
 
-	/**
-	 * @copydoc Plugin::getDisplayName
-	 */
-	function getDisplayName() {
-		return __('plugins.generic.epubViewer.name');
-	}
+    /**
+     * @copydoc Plugin::getDisplayName
+     */
+    public function getDisplayName()
+    {
+        return __('plugins.generic.epubViewer.name');
+    }
 
-	/**
-	 * @copydoc Plugin::getDescription
-	 */
-	function getDescription() {
-		return __('plugins.generic.epubViewer.description');
-	}
+    /**
+     * @copydoc Plugin::getDescription
+     */
+    public function getDescription()
+    {
+        return __('plugins.generic.epubViewer.description');
+    }
 
-	/**
-	 * Callback that renders the submission galley.
-	 * @param $hookName string
-	 * @param $args array
-	 * @return boolean
-	 */
-	function submissionCallback($hookName, $args) {
-		$request =& $args[0];
-		$application = Application::get();
-		switch ($application->getName()) {
-			case 'ojs2':
-				$issue =& $args[1];
-				$galley =& $args[2];
-				$submission =& $args[3];
-				$submissionNoun = 'article';
-				break;
-			default: throw new Exception('Unknown application!');
-		}
+    /**
+     * Callback that renders the submission galley.
+     * @param $hookName string
+     * @param $args array
+     * @return boolean
+     */
+    public function submissionCallback($hookName, $args)
+    {
+        $request =& $args[0];
+        $application = Application::get();
+        switch ($application->getName()) {
+            case 'ojs2':
+                $issue =& $args[1];
+                $galley =& $args[2];
+                $submission =& $args[3];
+                $submissionNoun = 'article';
+                break;
+            default: throw new Exception('Unknown application!');
+        }
 
-		if (!$galley) {
-			return false;
-		}
+        if (!$galley) {
+            return false;
+        }
 
-		$submissionFile = $galley->getFile();
-		if ($submissionFile->getData('mimetype') === 'application/epub+zip') {
-			$galleyPublication = null;
-			foreach ($submission->getData('publications') as $publication) {
-				if ($publication->getId() === $galley->getData('publicationId')) {
-					$galleyPublication = $publication;
-					break;
-				}
-			}
-			$templateMgr = TemplateManager::getManager($request);
-			$templateMgr->assign(array(
-				'displayTemplateResource' => $this->getTemplateResource('display.tpl'),
-				'pluginUrl' => $request->getBaseUrl() . '/' . $this->getPluginPath(),
-				'galleyFile' => $submissionFile,
-				'issue' => $issue,
-				'submission' => $submission,
-				'submissionNoun' => $submissionNoun,
-				'bestId' => $submission->getBestId(),
-				'galley' => $galley,
-				'currentVersionString' => $application->getCurrentVersion()->getVersionString(false),
-				'isLatestPublication' => $submission->getData('currentPublicationId') === $galley->getData('publicationId'),
-				'galleyPublication' => $galleyPublication,
-			));
-			$templateMgr->display($this->getTemplateResource('submissionGalley.tpl'));
-			return true;
-		}
+        $submissionFile = $galley->getFile();
+        if ($submissionFile->getData('mimetype') === 'application/epub+zip') {
+            $galleyPublication = null;
+            foreach ($submission->getData('publications') as $publication) {
+                if ($publication->getId() === $galley->getData('publicationId')) {
+                    $galleyPublication = $publication;
+                    break;
+                }
+            }
+            $templateMgr = TemplateManager::getManager($request);
+            $templateMgr->assign(array(
+                'displayTemplateResource' => $this->getTemplateResource('display.tpl'),
+                'pluginUrl' => $request->getBaseUrl() . '/' . $this->getPluginPath(),
+                'galleyFile' => $submissionFile,
+                'issue' => $issue,
+                'submission' => $submission,
+                'submissionNoun' => $submissionNoun,
+                'bestId' => $submission->getBestId(),
+                'galley' => $galley,
+                'currentVersionString' => $application->getCurrentVersion()->getVersionString(false),
+                'isLatestPublication' => $submission->getData('currentPublicationId') === $galley->getData('publicationId'),
+                'galleyPublication' => $galleyPublication,
+            ));
+            $templateMgr->display($this->getTemplateResource('submissionGalley.tpl'));
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	function viewCallback($hookName, $args) {
-		$submission =& $args[1];
-		$publicationFormat =& $args[2];
-		$submissionFile =& $args[3];
+    public function viewCallback($hookName, $args)
+    {
+        $submission =& $args[1];
+        $publicationFormat =& $args[2];
+        $submissionFile =& $args[3];
 
-		if ($submissionFile->getData('mimetype') == 'application/epub+zip') {
-			foreach ($submission->getData('publications') as $publication) {
-				if ($publication->getId() === $publicationFormat->getData('publicationId')) {
-					$filePublication = $publication;
-					break;
-				}
-			}
-			$request = Application::get()->getRequest();
-			$router = $request->getRouter();
-			$dispatcher = $request->getDispatcher();
-			$templateMgr = TemplateManager::getManager($request);
-			$templateMgr->assign(array(
-				'pluginUrl' => $request->getBaseUrl() . '/' . $this->getPluginPath(),
-				'isLatestPublication' => $submission->getData('currentPublicationId') === $publicationFormat->getData('publicationId'),
-				'filePublication' => $filePublication,
-			));
+        if ($submissionFile->getData('mimetype') == 'application/epub+zip') {
+            foreach ($submission->getData('publications') as $publication) {
+                if ($publication->getId() === $publicationFormat->getData('publicationId')) {
+                    $filePublication = $publication;
+                    break;
+                }
+            }
+            $request = Application::get()->getRequest();
+            $router = $request->getRouter();
+            $dispatcher = $request->getDispatcher();
+            $templateMgr = TemplateManager::getManager($request);
+            $templateMgr->assign(array(
+                'pluginUrl' => $request->getBaseUrl() . '/' . $this->getPluginPath(),
+                'isLatestPublication' => $submission->getData('currentPublicationId') === $publicationFormat->getData('publicationId'),
+                'filePublication' => $filePublication,
+            ));
 
-			$templateMgr->display($this->getTemplateResource('display.tpl'));
-			return true;
-		}
+            $templateMgr->display($this->getTemplateResource('display.tpl'));
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	function downloadCallback($hookName, $params) {
-		$submission =& $params[1];
-		$publicationFormat =& $params[2];
-		$submissionFile =& $params[3];
-		$inline =& $params[4];
+    public function downloadCallback($hookName, $params)
+    {
+        $submission =& $params[1];
+        $publicationFormat =& $params[2];
+        $submissionFile =& $params[3];
+        $inline =& $params[4];
 
-		$request = Application::get()->getRequest();
-		$mimetype = $submissionFile->getData('mimetype');
-		if ($mimetype == 'application/epub+zip' && $request->getUserVar('inline')) {
-			$inline = true;
-		}
-		
-		return false;
-	}
+        $request = Application::get()->getRequest();
+        $mimetype = $submissionFile->getData('mimetype');
+        if ($mimetype == 'application/epub+zip' && $request->getUserVar('inline')) {
+            $inline = true;
+        }
 
-	/**
-	 * Callback that renders the issue galley.
-	 * @param $hookName string
-	 * @param $args array
-	 * @return boolean
-	 */
-	function issueCallback($hookName, $args) {
-		$request =& $args[0];
-		$issue =& $args[1];
-		$galley =& $args[2];
+        return false;
+    }
 
-		$templateMgr = TemplateManager::getManager($request);
-		if ($galley && $galley->getFileType() == 'application/epub+zip') {
-			$application = Application::get();
-			$templateMgr->assign(array(
-				'displayTemplateResource' => $this->getTemplateResource('display.tpl'),
-				'pluginUrl' => $request->getBaseUrl() . '/' . $this->getPluginPath(),
-				'galleyFile' => $galley->getFile(),
-				'issue' => $issue,
-				'galley' => $galley,
-				'currentVersionString' => $application->getCurrentVersion()->getVersionString(false),
-				'isLatestPublication' => true,
-			));
-			$templateMgr->display($this->getTemplateResource('issueGalley.tpl'));
-			return true;
-		}
+    /**
+     * Callback that renders the issue galley.
+     * @param $hookName string
+     * @param $args array
+     * @return boolean
+     */
+    public function issueCallback($hookName, $args)
+    {
+        $request =& $args[0];
+        $issue =& $args[1];
+        $galley =& $args[2];
 
-		return false;
-	}
+        $templateMgr = TemplateManager::getManager($request);
+        if ($galley && $galley->getFileType() == 'application/epub+zip') {
+            $application = Application::get();
+            $templateMgr->assign(array(
+                'displayTemplateResource' => $this->getTemplateResource('display.tpl'),
+                'pluginUrl' => $request->getBaseUrl() . '/' . $this->getPluginPath(),
+                'galleyFile' => $galley->getFile(),
+                'issue' => $issue,
+                'galley' => $galley,
+                'currentVersionString' => $application->getCurrentVersion()->getVersionString(false),
+                'isLatestPublication' => true,
+            ));
+            $templateMgr->display($this->getTemplateResource('issueGalley.tpl'));
+            return true;
+        }
+
+        return false;
+    }
 }
 
+if (!PKP_STRICT_MODE) {
+    class_alias('\APP\plugins\generic\epubViewer\EpubViewerPlugin', '\EpubViewerPlugin');
+}
